f8d91a64aec660f7c4c37696747fa7f28a99bdc9
Michael Leung <mleung06@gmail.com>
2019-01-08 16:24:37 -0500

    Added purchaseDate field to Order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,12 +1,15 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
 const Order = db.define('order', {
-  purchased: {
+  isPurchased: {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
     allowNull: false
+  },
+  purchaseDate: {
+    type: Sequelize.DATE
   }
-});
+})
 
-module.exports = Order;
+module.exports = Order
