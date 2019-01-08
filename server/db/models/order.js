4f2efcf493e69131d26c97929cd2bb6e96a4b94c
Michael Leung <mleung06@gmail.com>
2019-01-08 15:50:33 -0500

    Created Order model

---
+++ server/db/models/order.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Order = db.define('order', {
+  purchased: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  }
+});
+
+module.exports = Order;
