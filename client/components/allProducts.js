00b0ddaf02c7b57c3be8aaf72384898b81d21136
Alex Kramer <alexkrameris@gmail.com>
2019-01-09 13:09:30 -0500

    built toolbar and card display with material-ui

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,25 +1,111 @@
 import React, {Component} from 'react'
 import {fetchProducts} from '../store'
 import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CameraIcon from '@material-ui/icons/PhotoCamera'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles'
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative'
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6
+  }
+})
 
 class AllProducts extends Component {
+  constructor(props) {
+    super(props)
+  }
   componentDidMount() {
     this.props.fetchProducts()
   }
-
   render() {
+    const {classes} = this.props
     return (
-      <div>
-        <ul>
-          {this.props.products.map(product => (
-            <li key={product.id}>
-              <div>
-                {product.name}
-                <img src={product.imageUrl} />
-              </div>
-            </li>
+      <div className={classNames(classes.layout, classes.cardGrid)}>
+        {/* End hero unit */}
+        <Grid container spacing={40}>
+          {this.props.products.map(card => (
+            <Grid item key={card.id} sm={6} md={4} lg={3}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image={card.imageUrl}
+                  title={card.name}
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {card.name}
+                  </Typography>
+                  <Typography>{card.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Purchase
+                  </Button>
+                  <Button size="small" color="primary">
+                    Add to Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
           ))}
-        </ul>
+        </Grid>
       </div>
     )
   }
@@ -36,5 +122,9 @@ const mapDispatchToProps = dispatch => {
     }
   }
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
+AllProducts.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(AllProducts)
+)
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,27 +3,48 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {AppBar, Typography, Button, Toolbar} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles'
+import {InsertEmoticon} from '@material-ui/icons'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const styles = theme => ({
+  appBar: {
+    position: 'relative'
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+})
+
+const Navbar = ({handleClick, isLoggedIn, classes}) => (
   <div>
-    <h1>Welcome to Ye Olde Joke Shop</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+        <InsertEmoticon className={classes.icon} />
+        <Typography variant="h6" color="inherit" noWrap>
+          Ye Olde Joke Shop
+        </Typography>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Button onClick={handleClick} className={classes.button}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Button className={classes.button}>Login</Button>
+            <Button className={classes.button}>Sign Up </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+
+    <nav />
     <hr />
   </div>
 )
@@ -44,13 +65,14 @@ const mapDispatch = dispatch => {
     }
   }
 }
-
-export default connect(mapState, mapDispatch)(Navbar)
-
 /**
  * PROP TYPES
  */
+
 Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
 }
+
+export default withStyles(styles)(connect(mapState, mapDispatch)(Navbar))
