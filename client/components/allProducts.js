4140afb45c7c81b099bb63e56c53450e45999fa5
Zach Rachlin <zachrach@gmail.com>
2019-01-09 16:54:56 -0500

    renamed backend jokes to be products and updated the route names accordingly, and started updating the allProducts page to route to single produc

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 import {fetchProducts} from '../store'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
-import CameraIcon from '@material-ui/icons/PhotoCamera'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
-import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import {withStyles} from '@material-ui/core/styles'
 
@@ -92,7 +89,12 @@ class AllProducts extends Component {
                   <Typography>{card.description}</Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" color="primary">
+                  <Button
+                    size="small"
+                    color="primary"
+                    component={Link}
+                    to={`/product/${card.id}`}
+                  >
                     View
                   </Button>
                   <Button size="small" color="primary">
@@ -126,5 +128,5 @@ AllProducts.propTypes = {
   classes: PropTypes.object.isRequired
 }
 export default withStyles(styles)(
-  connect(mapStateToProps, mapDispatchToProps)(AllProducts)
+  withRouter(connect(mapStateToProps, mapDispatchToProps)(AllProducts))
 )
--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,9 +1,84 @@
 import React, {Component} from 'react'
+import {fetchSingleProduct} from '../store'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles'
+
+const styles = theme => ({
+  icon: {
+    marginRight: theme.spacing.unit * 2
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  }
+})
 
 class SingleProduct extends Component {
+  componentDidMount() {
+    this.props.fetchSingleProduct()
+  }
+
   render() {
-    return <div />
+    const {classes, product} = this.props
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={product.imageUrl}
+          title={product.name}
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography>{product.description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Purchase
+          </Button>
+          <Button size="small" color="primary">
+            Add to Cart
+          </Button>
+        </CardActions>
+      </Card>
+    )
   }
 }
 
-export default SingleProduct
+const mapStateToProps = state => {
+  return {product: state.product.selectedProduct}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchSingleProduct: () => {
+      dispatch(fetchSingleProduct())
+    }
+  }
+}
+
+SingleProduct.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
+)
